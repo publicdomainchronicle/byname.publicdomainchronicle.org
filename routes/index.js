c5dd1c63d6395e618a4c8c20a395a32fb05e2a22
Kyle E. Mitchell <kyle@kemitchell.com>
2017-05-28 01:20:49 -0700

    Serve client-side scripts for guide and publish

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ var routes = module.exports = require('http-hash')()
 
 routes.set('/', serveFile('homepage.html'))
 routes.set('/styles.css', serveFile('styles.css'))
+
 routes.set('/guide', serveFile('guide.html'))
+routes.set('/guide.js', serveFile('guide.js'))
+
 routes.set('/publish', require('./publish'))
+routes.set('/publish.js', serveFile('publish.js'))
+
 routes.set('/key', require('./key'))
 routes.set('/publications', require('./publications'))
 routes.set('/publications/:digest', require('./publication'))
