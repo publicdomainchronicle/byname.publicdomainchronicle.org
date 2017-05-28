97fe86547ebb741b485c99659b47ec291ea50417
Kyle E. Mitchell <kyle@kemitchell.com>
2017-05-28 00:58:23 -0700

    Use streaming template for publications index

--- routes/publications.js
+++ routes/publications.js
@@ -5,6 +5,9 @@ var path = require('path')
 var pump = require('pump')
 var split2 = require('split2')
 var through2 = require('through2')
+var trumpet = require('trumpet')
+
+var TEMPLATE = path.join(__dirname, 'publications.html')
 
 module.exports = function (request, response, directory) {
   if (request.method === 'GET') {
@@ -28,29 +31,26 @@ module.exports = function (request, response, directory) {
           .pipe(response)
       } else if (type === 'text/html') {
         response.setHeader('Content-Type', 'text/html; charset=UTF-8')
-        response.write('<ol>')
+        var html = trumpet()
+        pump(html, response)
+        pump(fs.createReadStream(TEMPLATE), html)
         pump(
           fs.createReadStream(accessions),
           split2(),
-          through2.obj(
-            function (line, _, done) {
-              var split = line.split(',')
-              this.push(`
-                <li>
-                  <span class=date>${split[0]}</span>:
-                  <a href=/publications/${split[1]}>
-                    ${split[1]}
-                  </a>
-                </li>
-              `)
-              done()
-            },
-            function (done) {
-              this.push('</ol>')
-              done()
-            }
-          ),
-          response
+          through2.obj(function (line, _, done) {
+            var split = line.split(',')
+            var date = new Date(parseInt(split[0]))
+            this.push(`
+              <li>
+                <span class=date>${date.toISOString()}</span>:
+                <a href=/publications/${split[1]}>
+                  ${split[1]}
+                </a>
+              </li>
+            `)
+            done()
+          }),
+          html.select('#publications').createWriteStream()
         )
       }
     }
