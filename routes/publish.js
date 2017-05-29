1646f00121582f863a3ca25d9b78bc26598e378d
Kyle E. Mitchell <kyle@kemitchell.com>
2017-05-28 22:52:27 -0700

    List journals on publish form

--- routes/publish.js
+++ routes/publish.js
@@ -1,14 +1,38 @@
 var Busboy = require('busboy')
 var fs = require('fs')
 var methodNotAllowed = require('./method-not-allowed')
+var mustache = require('mustache')
 var path = require('path')
 var readKeypair = require('../read-keypair')
 var runSeries = require('run-series')
-var serveFile = require('./serve-file')
 var sodium = require('sodium-prebuilt').api
 var stringify = require('json-stable-stringify')
 
-var get = serveFile('publish.html')
+var JOURNALS = require('pct-minimum-documentation')
+  .map(function (element) {
+    return element.B
+  })
+  .sort()
+
+var TEMPLATE = path.join(
+  __dirname, '..', 'templates', 'publish.html'
+)
+
+var get = function (request, response) {
+  response.setHeader('Content-Type', 'text/html; charset=UTF-8')
+  fs.readFile(TEMPLATE, 'utf8', function (error, template) {
+    if (error) {
+      response.statusCode = 500
+      response.end()
+    } else {
+      response.end(
+        mustache.render(template, {
+          journals: JOURNALS
+        })
+      )
+    }
+  })
+}
 
 function post (request, response, directory) {
   readPostBody(request, function (error, fields) {
