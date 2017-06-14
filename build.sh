3f428b3fb303dde78fa55a6cb81ebec872bd3937
Kyle E. Mitchell <kyle@kemitchell.com>
2017-06-14 16:59:06 -0700

    Remove build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-partials=$(find static/partials)
-
-for template in static/*.mustache; do
-  node_modules/.bin/mustache static/mustache-view.json "$template" "${template%.mustache}.html"
-done
