1e4b664f158fe70504059efba6af4eefdc611c2e
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-18 17:45:08 -0700

    Fix absolute paths in <nav>

--- routes/partials/nav.js
+++ routes/partials/nav.js
@@ -4,8 +4,8 @@ module.exports = function () {
   return html`
     <nav>
       <ul>
-        <li><a href=/publish>Publish</a></li>
-        <li><a href=/accessions>Accessions</a></li>
+        <li><a href=publish>Publish</a></li>
+        <li><a href=accessions>Accessions</a></li>
         <li><a href=rss.xml>RSS Feed</a></li>
         <li><a href=declaration>Declaration</a></li>
         <li><a href=license>License</a></li>
