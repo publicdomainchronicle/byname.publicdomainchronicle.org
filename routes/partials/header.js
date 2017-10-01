a6736eff0da3849d0dac5192b1f8abda1dcd2342
Kyle E. Mitchell <kyle@kemitchell.com>
2017-10-01 12:09:59 -0700

    Make logo a link to search form

--- routes/partials/header.js
+++ routes/partials/header.js
@@ -3,7 +3,9 @@ var html = require('../html')
 module.exports = function () {
   return html`
 <header>
-  <img class=logo src=/logo.png alt=logotype>
+  <a href=/>
+    <img class=logo src=/logo.png alt=logotype>
+  </a>
 </header>
   `
 }
